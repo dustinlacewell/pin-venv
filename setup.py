4e91f2971704ef738784d6e6fac4a167e44b93ad
Dustin Lacewell <dlacewell@gmail.com>
2011-04-04 16:22:23 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import os
+
+from distutils.core import setup
+
+setup(
+    name='pin-venv',
+    version='0.0dev',
+    packages=['pin.plugins'],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README').read(),
+)

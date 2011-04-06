6fccd9a3d110d5ad3cd18d5af8712e89e01da5e3
Dustin Lacewell <dlacewell@gmail.com>
2011-04-06 10:57:49 -0500

    Added dependencies

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from distutils.core import setup
 
 setup(
     name='pin-venv',
-    version='0.4',
+    version='0.5',
     packages=['pin.plugins'],
-    requires=['pin', 'virtualenv'],
+    requires=['pin', 'capn', 'virtualenv'],
     author='Dustin Lacewell',
     author_email='dlacewell@gmail.com',
     url='https://github.com/dustinlacewell/pin-venv',
